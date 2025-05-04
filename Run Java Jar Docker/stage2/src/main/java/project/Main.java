6b1aaea3873b911a737d2bc2f79277a37ce5ef18
Eugene Trigubov <trigubov.eugene@gmail.com>
2025-05-04 18:37:22 +0200

    add files to stages

--- Run Java Jar Docker/stage2/src/main/java/project/Main.java
+++ Run Java Jar Docker/stage2/src/main/java/project/Main.java
@@ -1,7 +1,10 @@
 package project;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // Write your code here
     }
 }
