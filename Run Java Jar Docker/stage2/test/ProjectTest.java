4daecd8ea6c8325342423d6fa0b95c6c0d7e893b
Eugene Trigubov <trigubov.eugene@gmail.com>
2025-05-04 16:10:28 +0200

    add tests to stage 2

--- Run Java Jar Docker/stage2/test/ProjectTest.java
+++ Run Java Jar Docker/stage2/test/ProjectTest.java
@@ -2,9 +2,54 @@ import org.hyperskill.hstest.dynamic.DynamicTest;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 
-public class ProjectTest extends StageTest {
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+public class ProjectTest extends StageTest<Void> {
+
+
+    /**
+     * Checks if the 'MANIFEST.MF' file is present in the 'resources' and 'build_dir' directories
+     */
     @DynamicTest
-    CheckResult test() {
+    CheckResult test1() {
+        File manifestFile = new File("src/main/resources/META-INF/MANIFEST.MF");
+        if (!manifestFile.exists()) {
+            return CheckResult.wrong("The 'MANIFEST.MF' file was not found in the 'resources' directory.");
+        }
+
+        File manifestFileInBuild = new File("build_dir/MANIFEST.MF");
+        if (!manifestFileInBuild.exists()) {
+            return CheckResult.wrong("The 'MANIFEST.MF' file was not found in the 'build' directory.");
+        }
+
+        return CheckResult.correct();
+    }
+
+    /**
+     * Checks if the 'MANIFEST.MF' file contains the correct 'Manifest-Version' and 'Main-Class' attributes
+     */
+    @DynamicTest
+    CheckResult test2() {
+        File manifestFile = new File("build_dir/MANIFEST.MF");
+
+        String content;
+        try {
+            content = Files.readString(manifestFile.toPath(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            return CheckResult.wrong("Error reading 'MANIFEST.MF': " + e.getMessage());
+        }
+
+        if (!content.contains("Manifest-Version: 1.0")) {
+            return CheckResult.wrong("The 'MANIFEST.MF' file does not specify 'Manifest-Version: 1.0'.");
+        }
+
+        if (!content.contains("Main-Class: project.Main")) {
+            return CheckResult.wrong("The 'MANIFEST.MF' file does not specify 'Main-Class: project.Main'.");
+        }
+
         return CheckResult.correct();
     }
 }
